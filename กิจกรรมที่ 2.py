f8031276a417c75a61da4546aa30c7248e355900
Rujipas56 <rujipas2009@gmail.com>
2025-12-13 19:32:43 +0700

    Create กิจกรรมที่ 2.py

---
+++ กิจกรรมที่ 2.py
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="th">
+<head>
+    <meta charset="UTF-8">
+    <title>แสดงรูปภาพ HTML5</title>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+        }
+        .image-container {
+            display: flex;
+            width: 100%;
+        }
+        .image-container img {
+            width: 33.33%;
+            height: auto;
+        }
+    </style>
+</head>
+<body>
+
+    <div class="image-container">
+        <img src="image1.jpg" alt="รูปที่ 1">
+        <img src="image2.jpg" alt="รูปที่ 2">
+        <img src="image3.jpg" alt="รูปที่ 3">
+    </div>
+
+</body>
+</html>
